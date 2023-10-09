3c88adf88986cf3db778dd833882af34acc20569
Terry Mancey <terry.mancey@icloud.com>
2023-10-09 17:01:47 -0500

    Rename Brave Ads AdTypesReceived to AdsSummary

--- components/brave_ads/core/internal/account/statement/ads_summary_util_unittest.cc
+++ components/brave_ads/core/internal/account/statement/ads_summary_util_unittest.cc
@@ -3,8 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/brave_ads/core/internal/account/statement/ads_received_util.h"
+#include "brave/components/brave_ads/core/internal/account/statement/ads_summary_util.h"
 
+#include "base/containers/flat_map.h"
 #include "brave/components/brave_ads/core/internal/account/transactions/transactions_unittest_util.h"
 #include "brave/components/brave_ads/core/internal/common/unittest/unittest_base.h"
 #include "brave/components/brave_ads/core/internal/common/unittest/unittest_time_util.h"
@@ -13,9 +14,9 @@
 
 namespace brave_ads {
 
-class BraveAdsAdsReceivedUtilTest : public UnitTestBase {};
+class BraveAdsAdsSummaryUtilTest : public UnitTestBase {};
 
-TEST_F(BraveAdsAdsReceivedUtilTest, GetAdsReceivedForDateRange) {
+TEST_F(BraveAdsAdsSummaryUtilTest, GetAdsSummaryForDateRange) {
   // Arrange
   AdvanceClockTo(TimeFromString("5 November 2020", /*is_local=*/true));
 
@@ -60,11 +61,14 @@ TEST_F(BraveAdsAdsReceivedUtilTest, GetAdsReceivedForDateRange) {
   transactions.push_back(transaction_6);
 
   // Act & Assert
+  const base::flat_map<std::string, int32_t> expected_ads_summary = {
+      {"ad_notification", 2}, {"new_tab_page_ad", 1}, {"inline_content_ad", 1}};
   EXPECT_EQ(
-      4U, GetAdsReceivedForDateRange(transactions, from_time, DistantFuture()));
+      expected_ads_summary,
+      GetAdsSummaryForDateRange(transactions, from_time, DistantFuture()));
 }
 
-TEST_F(BraveAdsAdsReceivedUtilTest, DoNotGetAdsSummaryForDateRange) {
+TEST_F(BraveAdsAdsSummaryUtilTest, DoNotGetAdsSummaryForDateRange) {
   // Arrange
   AdvanceClockTo(TimeFromString("5 November 2020", /*is_local=*/true));
 
@@ -82,15 +86,24 @@ TEST_F(BraveAdsAdsReceivedUtilTest, DoNotGetAdsSummaryForDateRange) {
 
   AdvanceClockTo(TimeFromString("1 January 2021", /*is_local=*/true));
 
-  // Act & Assert
-  EXPECT_EQ(0U,
-            GetAdsReceivedForDateRange(transactions, Now(), DistantFuture()));
+  // Act
+  const base::flat_map<std::string, int32_t> ads_summary =
+      GetAdsSummaryForDateRange(transactions, Now(), DistantFuture());
+
+  // Assert
+  EXPECT_TRUE(ads_summary.empty());
 }
 
-TEST_F(BraveAdsAdsReceivedUtilTest, GetAdsSummaryForNoTransactions) {
-  // Act & Assert
-  EXPECT_EQ(0U, GetAdsReceivedForDateRange(/*transactions=*/{}, DistantPast(),
-                                           DistantFuture()));
+TEST_F(BraveAdsAdsSummaryUtilTest, GetAdsSummaryForNoTransactions) {
+  // Arrange
+  const TransactionList transactions;
+
+  // Act
+  const base::flat_map<std::string, int32_t> ads_summary =
+      GetAdsSummaryForDateRange(transactions, DistantPast(), DistantFuture());
+
+  // Assert
+  EXPECT_TRUE(ads_summary.empty());
 }
 
 }  // namespace brave_ads
